73d5d7d1298699485d9b21d0ae1f81f01f6d0051
brights0ng <sdcolson1@gmail.com>
2024-09-20 16:57:07 -0600

    Initial commit

---
+++ src/main/java/net/brights0ng/playground/PlaygroundClient.java
@@ -0,0 +1,10 @@
+package net.brights0ng.playground;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PlaygroundClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/brights0ng/playground/PlaygroundDataGenerator.java
@@ -0,0 +1,11 @@
+package net.brights0ng.playground;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PlaygroundDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
